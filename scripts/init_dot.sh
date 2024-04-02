6ecaef4f88d9a70eb12602004e27024a224f93f5
filmar <filippo.ufficiale@gmail.com>
2024-04-02 23:47:20 +0200

    fix

--- scripts/init_dot.sh
+++ scripts/init_dot.sh
@@ -7,7 +7,7 @@ if [ -e $HOME/git_projects/dot_files ]; then
     echo "neovim"
     ln -sfn $HOME/git_projects/dot_files/vim_files $HOME/.config/nvim
     echo "hyprland"
-    ln -sfn $HOME/git_projects/dot_files/hypr_files $HOME/hyprland
+    ln -sfn $HOME/git_projects/dot_files/hypr_files $HOME/.config/hypr
     echo "wezterm"
     ln -sfn $HOME/git_projects/dot_files/wezterm_files $HOME/.config/wezterm
     echo "waybar"
