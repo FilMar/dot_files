830539aa52f10ccad120623858584b4ce3e73b75
filmar <filippo.ufficiale@gmail.com>
2025-04-09 18:41:51 +0200

    restart emacs config

--- emacs_files/init.el
+++ emacs_files/init.el
@@ -1,70 +1,18 @@
-;; ====================
-;; 📦 Package Manager Setup
-;; ====================
-(require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
+(print "load filconfig")
 
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; ====================
-;; 🚀 use-package
-;; ====================
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-;; ====================
-;; 😈 Evil Mode
-;; ====================
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1))
-
-(defun my/vterm-setup ()
-  (display-line-numbers-mode -1)
-  (evil-local-mode -1))
-(add-hook 'vterm-mode-hook #'my/vterm-setup)
-
-
-(with-eval-after-load 'evil
-  (global-unset-key (kbd "C-x"))
-  (global-unset-key (kbd "M-x") )
-  (global-unset-key (kbd "C-c") )
-  (global-unset-key (kbd "C-u") )  ;; ad es. se non vuoi scroll
+;;(load (concat (getenv "CONFIG_HOME") "/emacs_files/package.el"))
+(let ((config (getenv "CONFIG_HOME")))
+    (if config
+        (load (concat config "/emacs_files/config/package.el"))
+    )
 )
-
-;; ====================
-;; vterm
-;; ====================
-(use-package vterm
-  :ensure t
-  :hook (vterm-mode . (lambda () (setq-local global-hl-line-mode nil))))
-
-(use-package multi-vterm)
-
-;; ====================
-;; 🎨 Catppuccin Theme
-;; ====================
-(use-package catppuccin-theme
-  :config
-  (setq catppuccin-flavor 'mocha) ;; scegli: 'latte, 'frappe, 'macchiato, 'mocha
-  (load-theme 'catppuccin t)
-  (catppuccin-reload))
-
 ;; ====================
 ;; 🧼 UI Clean-Up
 ;; ====================
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-()
 (setq inhibit-startup-screen t)
 (global-display-line-numbers-mode t)
 (show-paren-mode 1)
@@ -72,21 +20,21 @@
 (which-key-mode 1)
 (global-hl-line-mode 1)
 (setq ring-bell-function 'ignore)
-
-;; ====================
-;; 🔤 Font (opzionale)
-;; ====================
+(set-charset-priority 'unicode)
+(setq locale-coding-system 'utf-8
+      coding-system-for-read 'utf-8
+      coding-system-for-write 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(defun ab/enable-line-numbers ()
+    "Enable relative line numbers"
+    (interactive)
+    (display-line-numbers-mode)
+    (setq display-line-numbers 'relative))
+(add-hook 'prog-mode-hook #'ab/enable-line-numbers)
 (set-face-attribute 'default nil :font "FiraCode Nerd Font" :height 110)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(warning-suppress-log-types '((native-compiler))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
