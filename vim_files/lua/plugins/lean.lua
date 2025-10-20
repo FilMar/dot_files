c534cdc5e01deef60b7227890a60c3ea3f28da83
filmar <filippo.ufficiale@gmail.com>
2025-10-20 15:24:24 +0200

    some changes in lean and more

--- vim_files/lua/plugins/lean.lua
+++ vim_files/lua/plugins/lean.lua
@@ -6,15 +6,6 @@ return {
     'neovim/nvim-lspconfig',
     'nvim-lua/plenary.nvim',
 
-    -- optional dependencies:
-
-    -- a completion engine
-    --    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
-
-    -- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
-    -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
-    -- 'andrewradev/switch.vim',        -- for switch support
-    -- 'tomtom/tcomment_vim',           -- for commenting
   },
 
   ---@type lean.Config
@@ -26,6 +17,8 @@ return {
       autoopen = true,
       width = 50,
       height = 20,
+      orientation = "horizontal",
+      horizontal_position = "bottom"
     },
   },
 
