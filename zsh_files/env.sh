273b67dd438066ec0c2500bfb27c04d1561c3803
filmar <filippo.ufficiale@gmail.com>
2026-07-05 15:27:03 +0200

    mise activation

--- zsh_files/env.sh
+++ zsh_files/env.sh
@@ -35,3 +35,10 @@ if [[ -e ~/.profile ]]; then
     source ~/.profile
 fi
 
+# mise
+if command -v mise &>/dev/null; then
+    eval "$(mise activate zsh)"
+elif [[ -x "$HOME/.local/bin/mise" ]]; then
+    eval "$($HOME/.local/bin/mise activate zsh)"
+fi
+
